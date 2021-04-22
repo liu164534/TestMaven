6f65fd589f07ce2b14f82a23bd7511ecf65183cd
bizvane <liuxinpeng@bizvane.cn>
2021-04-22 14:02:20 +0800

    创建

---
+++ src/main/java/com/test/entity/RequestVo.java
@@ -0,0 +1,20 @@
+package com.test.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @program: TestMaven
+ * @description:
+ * @author: Liu Xinpeng
+ * @create: 2021-04-22 10:33
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RequestVo {
+
+}
