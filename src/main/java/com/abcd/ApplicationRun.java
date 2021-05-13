756f88852519c2cddd8255e79e81859fcd18e3db
bizvane <liuxinpeng@bizvane.cn>
2021-05-13 17:08:44 +0800

    集成Nacos

---
+++ src/main/java/com/abcd/ApplicationRun.java
@@ -0,0 +1,22 @@
+package com.abcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: TestMaven
+ * @description: 启动类
+ * @author: Liu Xinpeng
+ * @create: 2021-04-22 16:50
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ApplicationRun {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRun.class, args);
+    }
+
+}
--- src/main/java/com/abcd/entity/RequestVo.java
+++ src/main/java/com/abcd/entity/RequestVo.java
@@ -1,4 +1,4 @@
-package com.test.entity;
+package com.abcd.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
